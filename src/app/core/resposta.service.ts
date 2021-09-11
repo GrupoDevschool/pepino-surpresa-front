253d31fa300b3768ca1eb53ee886a485c5d395fc
Thales M P Macena <thales.macena@akross.com.br>
2021-09-11 13:12:00 -0300

    crud de respostas

---
+++ src/app/core/resposta.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Resposta } from '../shared/model/Resposta';
+
+const url = 'http://localhost:8080/respostas/';
+@Injectable({
+  providedIn: 'root'
+})
+export class RespostaService {
+
+  constructor(private http: HttpClient) { }
+
+  save(resposta: Resposta): Observable<Resposta> {
+    return this.http.post<Resposta>(url, resposta);
+  }
+
+  edit(resposta: Resposta): Observable<Resposta> {
+    return this.http.put<Resposta>(url + resposta.id, resposta);
+  }
+
+  list(): Observable<Resposta[]> {
+    return this.http.get<Resposta[]>(url);
+  }
+
+  listByArea(id: number): Observable<Resposta[]> {
+    return this.http.get<Resposta[]>(url + `?areaId=${id}`);
+  }
+
+  show(id: number): Observable<Resposta> {
+    return this.http.get<Resposta>(url + id);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(url + id);
+  }
+
+}
---
+++ src/app/resposta/resposta.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RespostaComponent } from './resposta.component';
+
+describe('RespostaComponent', () => {
+  let component: RespostaComponent;
+  let fixture: ComponentFixture<RespostaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RespostaComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RespostaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

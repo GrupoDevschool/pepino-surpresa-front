6edb0e1c5834939e3236348faa1eae89e7e9a2da
Niasi Magalhães <not@comic.com>
2021-09-11 15:17:09 -0300

    Service de Turma

---
+++ src/app/core/turma.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Turma } from '../shared/model/Turma';
+
+const url = 'http://localhost:8080/turmas/';
+@Injectable({
+  providedIn: 'root'
+})
+export class TurmaService {
+
+  constructor(private http: HttpClient) { }
+
+  save(turma: Turma): Observable<Turma> {
+    return this.http.post<Turma>(url, turma);
+  }
+
+  edit(turma: Turma): Observable<Turma> {
+    return this.http.put<Turma>(url + turma.id, turma);
+  }
+
+  list(): Observable<Turma[]> {
+    return this.http.get<Turma[]>(url);
+  }
+
+  show(id: number): Observable<Turma> {
+    return this.http.get<Turma>(url + id);
+  }
+
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(url + id);
+  }
+
+}

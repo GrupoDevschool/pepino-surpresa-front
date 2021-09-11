351a3a2ea0f5e182d434ef347bdb0e12404f7bb4
Niasi Magalhães <not@comic.com>
2021-09-11 15:00:50 -0300

    Aula ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AlunoComponent } from "./aluno/aluno.component";
+import { AulaComponent } from "./aula/aula.component";
 import { AvaliacaoComponent } from "./avaliacao/avaliacao.component";
 import { DisciplinaComponent } from "./disciplina/disciplina.component";
 import { TrilhaComponent } from "./trilha/trilha.component";
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'avaliacoes', component: AvaliacaoComponent },
   { path: 'trilhas', component: TrilhaComponent },
   { path: 'disciplinas', component: DisciplinaComponent },
+  { path: 'aulas', component: AulaComponent}
 ];
 
 @NgModule({

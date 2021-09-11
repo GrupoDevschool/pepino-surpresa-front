1941bd4a876d7e660e16315f5b202b8d0949a6e7
Niasi Magalhães <not@comic.com>
2021-09-11 14:42:09 -0300

    Funções do typescript de Aula

--- src/app/aula/aula.component.ts
+++ src/app/aula/aula.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AulaService } from '../core/aula.service';
+import { Aula } from '../shared/model/Aula';
 
 @Component({
   selector: 'app-aula',
@@ -7,9 +9,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AulaComponent implements OnInit {
 
-  constructor() { }
+  aulas: Aula[];
+  aula: Aula = {} as Aula;
+  updatedAula: Aula = {} as Aula;
+
+  modalIsVisible: boolean = false;
+
+  constructor(private aulaService: AulaService) {
+  }
 
   ngOnInit(): void {
+    this.reloadData();
+  }
+
+  reloadData() {
+    this.aulaService.list().subscribe((aulas) => {
+      this.aulas = aulas;
+    });
+  }
+
+  save() {
+    this.aulaService.save(this.aula).subscribe(
+      avaliacao => this.aulas.push(avaliacao),
+      error => console.log(error)
+    )
+  }
+
+  edit() {
+    this.aulaService.edit(this.updatedAula).subscribe(() => {
+      this.reloadData();
+      this.closeModal();
+      },
+      error => {
+        console.log(error);
+        this.closeModal();
+      }
+    )
+  }
+
+  delete(id: number) {
+    this.aulaService.delete(id).subscribe(() => {
+      // a função filter recebe uma função a ser executada em cada elemento do array
+      // nesse caso, estou salvando no array de avaliações as avaliações que tem id diferente do que recebi
+        this.aulas = this.aulas.filter((element) => element.id !== id)
+      }
+    )
+  }
+
+  openModal(aula: Aula) {
+    this.updatedAula = Object.assign({}, aula);
+    this.modalIsVisible = this.modalIsVisible = true;
+  }
+
+  closeModal() {
+    this.modalIsVisible = this.modalIsVisible = false;
   }
 
 }

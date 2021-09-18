dbb53bc8169f1f4ce2f46b76769427127e1cdf48
Thales M P Macena <thales.macena@akross.com.br>
2021-09-17 22:35:08 -0300

    Finalização das perguntas

--- src/app/pergunta/pergunta.component.ts
+++ src/app/pergunta/pergunta.component.ts
@@ -3,7 +3,7 @@ import { IDropdownSettings } from 'ng-multiselect-dropdown';
 import { RespostaService } from '../core/resposta.service';
 import { Area } from '../shared/model/Area';
 import { Disciplina } from '../shared/model/Disciplina';
-import { Pergunta } from '../shared/model/Pergunta';
+import { Pergunta, PerguntaDTO } from '../shared/model/Pergunta';
 import { AreaService } from './../core/area.service';
 import { DisciplinaService } from './../core/disciplina.service';
 import { PerguntaService } from './../core/pergunta.service';
@@ -22,15 +22,17 @@ export class PerguntaComponent implements OnInit {
   areas: Area[];
 
   pergunta: Pergunta = {} as Pergunta;
-  disciplina: Disciplina[];
+  possiveisRespostas: Resposta[] = [];
+  disciplina: Disciplina[] = [];
   area: Area[];
 
   updatedPergunta: Pergunta = {} as Pergunta;
-  updatedDisciplina: Disciplina;
+  updatedDisciplina: Disciplina[];
+  updatedArea: Area[];
+  updatedRespostas: Resposta[];
 
-  possiveisRespostas: Resposta[] = [];
-
-  modalIsVisible: boolean = false;
+  updateModalIsVisible: boolean = false;
+  responseModalIsVisible: boolean = false;
 
   dropdownDisciplinaSettings: IDropdownSettings = {};
   dropdownAreaSettings: IDropdownSettings = {};
@@ -139,23 +141,81 @@ export class PerguntaComponent implements OnInit {
         this.possiveisRespostas = [];
 
         if (this.disciplina.length) {
+          this.disciplina = Array.of(this.disciplinas.find((element) => element.id === this.disciplina[0].id));
+
           this.area = Array.of(this.disciplina[0].areas[0]);
+
           this.getRespostaByDisciplinaId(this.disciplina[0].id);
+
         } else if (this.area.length){
           this.getRespostaByAreaId(this.area[0].id)
         } else {
           this.getRespostas();
         }
         break;
-      case "resposta":
+      case "updatedArea":
+        this.updatedRespostas = [];
+
+        this.updatedDisciplina = [];
+
+        if (this.updatedArea.length) {
+          this.getRespostaByAreaId(this.updatedArea[0].id)
+          this.getDisciplinaByAreaId(this.updatedArea[0].id);
+        } else {
+          this.getRespostas();
+          this.getDisciplinas();
+        }
+
+        break;
+      case "updatedDisciplina":
+        this.updatedRespostas = [];
+
+        if (this.updatedDisciplina.length) {
+          this.updatedDisciplina = Array.of(this.disciplinas.find((element) => element.id === this.updatedDisciplina[0].id));
+
+          this.updatedArea = Array.of(this.updatedDisciplina[0].areas[0]);
+
+          this.getRespostaByDisciplinaId(this.updatedDisciplina[0].id);
+
+        } else if (this.updatedArea.length){
+          this.getRespostaByAreaId(this.updatedArea[0].id)
+        } else {
+          this.getRespostas();
+        }
         break;
     }
   }
 
   save() {
-    this.PerguntaService.save(this.pergunta).subscribe(
+    const newPergunta: PerguntaDTO = {
+      enunciado: this.pergunta.enunciado,
+      disciplinaId: this.disciplina[0].id,
+      respostas: this.possiveisRespostas
+    }
+
+    this.PerguntaService.save(newPergunta).subscribe(
       pergunta => this.perguntas.push(pergunta),
       error => console.log(error)
+    )
+  }
+
+  edit() {
+    const newPergunta: PerguntaDTO = {
+      id: this.updatedPergunta.id,
+      enunciado: this.updatedPergunta.enunciado,
+      disciplinaId: this.updatedDisciplina[0].id,
+      respostas: this.updatedRespostas
+    }
+
+    this.PerguntaService.edit(newPergunta).subscribe(
+      () => {
+        this.reloadData();
+        this.closeUpdateModal();
+      },
+      error => {
+        console.log(error);
+        this.closeUpdateModal();
+      }
     )
   }
 
@@ -164,5 +224,31 @@ export class PerguntaComponent implements OnInit {
           this.perguntas = this.perguntas.filter((element) => element.id !== id)
       }
     )
+  }
+
+  openResponseModal(pergunta: Pergunta) {
+    this.updatedPergunta = Object.assign({}, pergunta);
+    this.updatedDisciplina = Array.of(pergunta.disciplina);
+    this.updatedRespostas = Object.assign([], pergunta.respostas);
+    this.updatedArea = Array.of(pergunta.disciplina.areas[0]);
+
+    this.responseModalIsVisible = true;
+  }
+
+  closeResponseModal() {
+    this.responseModalIsVisible = false;
+  }
+
+  openUpdateModal(pergunta: Pergunta) {
+    this.updatedPergunta = Object.assign({}, pergunta);
+    this.updatedDisciplina = Array.of(pergunta.disciplina);
+    this.updatedRespostas = Object.assign([], pergunta.respostas);
+    this.updatedArea = Array.of(pergunta.disciplina.areas[0]);
+
+    this.updateModalIsVisible = true;
+  }
+
+  closeUpdateModal() {
+    this.updateModalIsVisible = false;
   }
 }

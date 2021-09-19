99625281585405b84bb75db5a5d63eb55f242b5e
Thales M P Macena <thales.macena@akross.com.br>
2021-09-19 17:37:53 -0300

    Questao

---
+++ src/app/shared/model/Questao.ts
@@ -0,0 +1,12 @@
+
+/**
+* Questao é o modelo que representa uma questão
+* @field numero é o numero da questão em uma avaliação
+* @field PerguntaResposta é um array que em sua primeira posição tem o id da pergutam e nas posições seguintes o id das respostas da questão
+*/
+export interface Questao {
+  id: number;
+  avaliacaoId: number;
+  numero: number;
+  perguntaResposta: number[]
+}
